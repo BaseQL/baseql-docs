183956b039420fba3640dc5994d5e9d249a8b198
Ben Orozco <benoror@gmail.com>
2020-11-30 23:36:03 -0600

    Feat[LiveQuerying]: Query using SWR + graphql-request

--- components/query-playground.js
+++ components/query-playground.js
@@ -1,9 +1,19 @@
-import QueryEditor from '../../components/query-editor';
+import QueryEditor from '../components/query-editor';
+import { request } from 'graphql-request'
+import useSWR from 'swr'
+
+const fetcher = query =>
+  request('https://api.baseql.com/airtable/graphql/appuzDcQEvfnkzXjD', query)
 
 export default function QueryPlayground({query, disabled = false}) {
+  const { data, error } = useSWR(query, fetcher)
+
   return (
     <div style={{display: 'flex'}}>
       <QueryEditor query={query} disabled={disabled} />
+      <pre>
+        <code  style={{fontSize: '10px'}}>{JSON.stringify(data.people, undefined, 2)}</code>
+      </pre>
     </div>
   )
 }
