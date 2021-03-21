b1d1769994d3d6adce8e08761e1db11d39554250
Ben Orozco <benoror@gmail.com>
2021-03-20 23:36:55 -0600

    Ref[QueryPlayground]: Extract presentational/dumb component

--- components/query-playground.js
+++ components/query-playground.js
@@ -1,19 +1,6 @@
 import { request } from 'graphql-request'
 import useSWR from 'swr'
-import QueryEditor from './query-editor';
-import QueryHighlighter from './query-highlighter'
-import Loading from './loading'
-import theme from 'prism-react-renderer/themes/github'
-
-const styles = {
- results: {
-  whiteSpace: 'pre-wrap',
-  flex: 1,
-  margin: '4px',
-  padding: '10px',
-  fontFamily: '"Dank Mono", "Fira Code", monospace',
- } 
-}
+import Query from './query'
 
 const fetcher = query =>
   request('https://api.baseql.com/airtable/graphql/appuzDcQEvfnkzXjD', query)
@@ -22,19 +9,11 @@ export default function QueryPlayground({query, disabled = false}) {
   const { data, error } = useSWR(query, fetcher)
 
   return (
-    <div style={{width: '100%', maxHeight: '300px', display: 'flex', alignItems: 'stretch'}}>
-      <QueryEditor query={query} disabled={disabled}/>
-      <pre style={styles.results}>
-        {!data && !error && <Loading />}
-        {!!data && (
-          <QueryHighlighter code={JSON.stringify(data, undefined, 2)} theme={theme} />
-        )}
-        {!!error && (
-          <div>❌ Error: 
-            <QueryHighlighter code={JSON.stringify(error, undefined, 2)} theme={theme} />
-          </div>
-        )}
-      </pre>
-    </div>
+    <Query
+      query={query}
+      code={JSON.stringify(data, undefined, 2)}
+      error={error}
+      disabled={disabled}
+    />
   )
 }
--- components/query.js
+++ components/query.js
@@ -1,3 +1,33 @@
-export default function Query({children}) {
-  return children
+import QueryEditor from './query-editor';
+import QueryHighlighter from './query-highlighter'
+import Loading from './loading'
+import theme from 'prism-react-renderer/themes/github'
+
+const styles = {
+ results: {
+  whiteSpace: 'pre-wrap',
+  flex: 1,
+  margin: '4px',
+  padding: '10px',
+  fontFamily: '"Dank Mono", "Fira Code", monospace',
+ } 
+}
+
+export default function Query({query, code, error, disabled = false}) {
+  return (
+    <div style={{width: '100%', maxHeight: '300px', display: 'flex', alignItems: 'stretch'}}>
+      <QueryEditor query={query} disabled={disabled}/>
+      <pre style={styles.results}>
+        {!code && !error && <Loading />}
+        {!!code && (
+          <QueryHighlighter code={code} theme={theme} />
+        )}
+        {!!error && (
+          <div>❌ Error: 
+            <QueryHighlighter code={code} theme={theme} />
+          </div>
+        )}
+      </pre>
+    </div>
+  )
 }
