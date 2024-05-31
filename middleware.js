3284756e070e5dd485190e1e362f98b4ee9c59c8
DJ Seeds <dseeds14@gmail.com>
2024-05-31 13:13:54 +0200

    Add page view analytics

---
+++ middleware.js
@@ -0,0 +1,21 @@
+import { NextResponse } from 'next/server'
+
+import analyticsMiddleware from "./lib/middleware/analyticsMiddleware"
+
+export async function middleware(request) {
+    const response = NextResponse.next();
+    return await analyticsMiddleware(request, response);
+}
+
+export const config = {
+    matcher: [
+      /*
+       * Match all request paths except for the ones starting with:
+       * - api (API routes)
+       * - _next/static (static files)
+       * - _next/image (image optimization files)
+       * - *.ico,*.png,*.svg (image files)
+       */
+      '/((?!api|_next/static|_next/image|.*.ico|.*.png|.*.svg).*)',
+    ],
+  }
