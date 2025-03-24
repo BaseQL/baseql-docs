3e4c328c498f1b8b4dca18c5daaae9e2058b537e
DJ Seeds <dseeds14@gmail.com>
2025-03-23 19:26:08 -0600

    Code review tweaks

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import { DocsThemeConfig, useConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: (
@@ -13,7 +13,7 @@ const config: DocsThemeConfig = {
   project: {
     link: 'https://github.com/BaseQL'
   },
-  docsRepositoryBase: 'https://github.com/BaseQL/baseql-docs/tree/main/pages',
+  docsRepositoryBase: 'https://github.com/BaseQL/baseql-docs/tree/main',
   sidebar: {
     defaultMenuCollapseLevel: 1,
     autoCollapse: true,
@@ -23,42 +23,38 @@ const config: DocsThemeConfig = {
     backToTop: true
   },
   darkMode: true,
-  banner: {
-    key: 'nextra-banner',
-    content: (
-      <span>
-        Welcome to BaseQL Documentation! 🚀
-      </span>
-    )
-  },
   footer: {
     content: (
       <span>Copyright {new Date().getFullYear()} © <a href="https://baseql.com/" target="_blank" rel="noopener">BaseQL</a></span>
     )
   },
-  head: (
-    <>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-163884111-2"></script>
-      <script dangerouslySetInnerHTML={{
-        __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'UA-163884111-2');
-        `
-      }} />
-      <meta name="msapplication-TileColor" content="#4c51bf" />
-      <meta name="theme-color" content="#4c51bf" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="BaseQL: GraphQL for Airtable" />
-      <meta name="og:description" content="BaseQL: GraphQL for Airtable" />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content="@BaseQL" />
-      <meta name="og:title" content="BaseQL: GraphQL for Airtable" />
-      <meta name="apple-mobile-web-app-title" content="BaseQL" />
-    </>
-  )
+  head() {
+    const { title } = useConfig();
+    return (
+      <>
+        <title>{(title || 'BaseQL') + ' - BaseQL Docs'}</title>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-163884111-2"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-163884111-2');
+          `
+        }} />
+        <meta name="msapplication-TileColor" content="#4c51bf" />
+        <meta name="theme-color" content="#4c51bf" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="Content-Language" content="en" />
+        <meta name="description" content="BaseQL: GraphQL for Airtable" />
+        <meta name="og:description" content="BaseQL: GraphQL for Airtable" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@BaseQL" />
+        <meta name="og:title" content="BaseQL: GraphQL for Airtable" />
+        <meta name="apple-mobile-web-app-title" content="BaseQL" />
+      </>
+    )
+  }
 }
 
 export default config 
